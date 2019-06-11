717c4fac8c8d8d94f6a69919f28cca1cf1e5c02e
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-11 13:20:34 -0300

    remove file

--- headerDash.R
+++
@@ -1,8 +0,0 @@
-headerDash <- function(title_tag = "Dashboard Shiny") {
-  HTML(sprintf('
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>%s</title>
-              ', title_tag))
-}
