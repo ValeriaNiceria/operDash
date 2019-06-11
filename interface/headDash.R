53119dea82faf847888f433ee186e35b1dbefc3a
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-11 13:56:41 -0300

    head

--- interface/headDash.R
+++ interface/headDash.R
@@ -1,13 +1,17 @@
 headerDash <- function(..., title_tag = "Dashboard Shiny") {
-  HTML(sprintf('
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>%s</title>
-    
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-              ', title_tag))
+  # HTML(sprintf(''))
+  
+  tags$html(
+    tags$head(
+      tags$title(title_tag),
+      tags$link(rel = "stylesheet", type = "text/css", href = "css/bootstrap.min.css"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "font-awesome/css/font-awesome.css"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "css/animate.css"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css"),
+      list(...)
+    )
+  )
+  
+  # lapply(list(...), function(x) tagList(fluidRow(x), br()))
+  
 }
