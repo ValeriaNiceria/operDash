42c0793efb56c9952b3b96c14d466e65cc832b27
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-12 16:52:59 -0300

    menu

--- app.R
+++ app.R
@@ -1,6 +1,103 @@
 source("./imports.R")
+source("./interface_imports.R")
 
-source("./ui.R")
+
+menuDash = menuDash(title_site = "Dashboard",
+                    menuLeft(
+                      menuItem(text = "ibox", tabName = "ibox"),
+                      menuItem(text = "Widget", icon = "fa fa-th-large", tabName = "widget"),
+                      menuDropdown(menuItem(text = "TESTE2", icon = "fa fa-sign-out"))
+                    ),
+                    
+                    menuRight(
+                      menuItem(text = "TESTE2", icon = "fa fa-sign-out"),
+                      menuItem(text = "TESTE2", icon = "fa fa-sign-out"),
+                      menuItem(text = "TESTE2", icon = "fa fa-sign-out")
+                    )
+)
+
+
+ui <- htmlTemplate("./template/index.html",
+                   
+                   headDash = headerDash(),
+                   
+                   menuDash = menuDash,
+                   
+                   
+                   contentDash = contentDash(
+                     
+                     # ibox
+                     tabItem(tabName = "ibox",
+                             fluidRow(column(width = 4,
+                                             ibox(
+                                               iboxTitle(
+                                                 iboxTools(label())
+                                               )
+                                             )
+                             ),
+                             
+                             column(width = 8,
+                                    ibox(
+                                      iboxTitle(
+                                        iboxTools(collapseLink(),
+                                                  closeLink())
+                                      ),
+                                      iboxContent(
+                                        
+                                      )
+                                    )
+                             )
+                             )  
+                     ),
+                     
+                     # Widgets 
+                     tabItem(tabName = "widget",
+                             fluidRow(
+                               column(
+                                 width = 3,
+                                 widget(type = "default")),
+                               column(
+                                 width = 3,
+                                 widget(type = "primary")),
+                               column(
+                                 width = 3,
+                                 widget(type = "info")),
+                               column(
+                                 width = 3,
+                                 widget(type = "warning"))
+                             ),
+                             
+                             fluidRow(
+                               column(
+                                 width = 3,
+                                 widget(type = "danger")
+                               ),
+                               column(
+                                 width = 2,
+                                 widgetSimples(type = "warning")
+                               ),
+                               column(
+                                 width = 2,
+                                 widgetSimples(type = "primary")
+                               ),
+                               column(
+                                 width = 2,
+                                 widgetSimples(type = "info")
+                               ),
+                               column(
+                                 width = 2,
+                                 widgetSimples(type = "danger")
+                               )
+                             )
+                             
+                     )  
+                   ),
+                   
+                   
+                   footerDash = footerDash()
+                   
+)
+
 source("./server.R")
 
 shinyApp(ui, server)
