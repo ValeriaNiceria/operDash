27bf5cda22a394309aae13f6a1ed5d842efb4508
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-14 12:57:20 -0300

    remove comments

--- interface/box.R
+++ interface/box.R
@@ -40,16 +40,3 @@ box <- function(...) {
   tags$div(class = "ibox",
            list(...))
 }
-
-
-# <div class="ibox ">
-#   <div class="ibox-title">
-#     <span class="label label-success float-right">Monthly</span>
-#       <h5>Views</h5>
-#       </div>
-#       <div class="ibox-content">
-#         <h1 class="no-margins">386,200</h1>
-#           <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
-#             <small>Total views</small>
-#             </div>
-#             </div>
