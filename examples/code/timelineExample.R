1786b20cdb5b49e247eab8731500d9db700c5b66
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-24 08:09:53 -0300

    timeline

--- examples/code/timelineExample.R
+++ examples/code/timelineExample.R
@@ -1,13 +1,25 @@
 codeTimelineExample <-
 tabItem(tabName = "codeTimeline",
-    tags$h3(class="font-bold", "timelineBlock()"),
+        fluidRow(
+          column(
+            width = 6,
+            box(
+              boxTitle(title = "timelineBlock()", boxTools(collapseLink())),
+              boxContent(
 HTML('
 <pre class="R">
 timelineBlock(...)
 </pre>
-'),
-    tags$h3(class="font-bold", "timelineItem()"),
-    tags$small(strong("statusIcon =>"), span(class="bg-muted", "muted"), span(class="bg-primary", "primary"), span(class="bg-success", "success"), span(class="bg-info", "info"), span(class="bg-warning", "warning"), span(class="bg-danger", "danger")),
+')
+              )
+            )
+          ),
+          column(
+            width = 6,
+            box(
+              boxTitle(title = "timelineItem()", boxTools(collapseLink())),
+              boxContent(
+                tags$small(strong("statusIcon =>"), span(class="bg-muted", "muted"), span(class="bg-primary", "primary"), span(class="bg-success", "success"), span(class="bg-info", "info"), span(class="bg-warning", "warning"), span(class="bg-danger", "danger")),
 HTML('
 <pre class="R">
 timelineItem(
@@ -18,34 +30,48 @@ timelineItem(
   time = "20/06/2019"
 )
 </pre>
-'),
-
-    tags$h2(class="font-bold", "Exemplo"),
+')
+              )
+            )
+          )
+        ),
+        fluidRow(
+          column(
+            width = 12,
+            box(
+              boxTitle(title = "Exemplo", boxTools(collapseLink())),
+              boxContent(
 HTML('
 <pre class="R">
 box(
   boxContent(
     timelineBlock(
       timelineItem(
-       icon = icon("briefcase"),
-       statusIcon = "primary",
-       title = "Título",
-       content = tags$div(p("conteúdo teste timeline 2"), 
-                          br(),
-                          appButton(label = "mais...", 
-                                  icon = icon("plus"), 
-                                  status = "info")),
-       time = tags$div("Today",
-                      br(), 
-                      icon("clock"), 
-                      small("11:40"))
+        icon = icon("briefcase"),
+        statusIcon = "primary",
+        title = "Título",
+        content = tags$div(p("conteúdo teste timeline 2"), 
+                    br(),
+                    appButton(label = "mais...", 
+                              icon = icon("plus"), 
+                              status = "info"
+                    )
+                  ),
+        time = tags$div("Today",
+                br(), 
+                icon("clock"), 
+                small("11:40")
+              )
       )
     )
   )
 )
 </pre>
 ')
-  
+              )
+            )
+          )
+        )
 )
 
 # fluidRow(
