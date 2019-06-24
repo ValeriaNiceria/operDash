dc777aa488f8db7c71b2a3d6bc859e633e55ee17
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-23 23:45:07 -0300

    valueBox

--- examples/valueBoxElement.R
+++ examples/valueBoxElement.R
@@ -35,5 +35,68 @@ valueBoxElement <-
                            value = "56",
                            status = "primary")
             )
-          )
+          ),
+          fluidRow(
+            column(
+              width = 12,
+HTML('
+<pre class="R">
+fluidRow(
+  column(
+    width = 3,
+    valueBox(
+      status = "default"
+    )
+  ),
+  column(
+    width = 3,
+    valueBox(
+      status = "primary"
+    )
+  ),
+  column(
+    width = 3,
+    valueBox(
+      status = "info"
+    )
+  ),
+  column(
+    width = 3,
+    valueBox(
+      status = "warning"
+    )
+  )
+),
+
+fluidRow(
+  column(
+    width = 3,
+    valueBox(
+      status = "danger"
+    )
+  ),
+  column(
+    width = 3,
+    infoMetric(
+      title = "Income",
+      subTitle = "Total income", 
+      value = "999,99", 
+      percent = "77%", 
+      icon = icon("bolt")
+    )
+  ),
+  column(
+    width = 3,
+    infoProgress(
+      title = "Título",
+      subTitle = "Sub título",
+      value = "56",
+      status = "primary"
+    )
+  )
+)
+</pre>
+')
+              )
+            )
   )
