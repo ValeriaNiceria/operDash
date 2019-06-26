6ba3400395426b5aaf674b3f7a5a18769ba2366e
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-26 14:42:12 -0300

    notification

--- R/notifications.R
+++ R/notifications.R
@@ -1,3 +1,16 @@
+#' @title Dashboard notification item
+#' @name notificationItem
+#'
+#'
+#' @param icon A icon.
+#' @param title A title.
+#' @param message A message.
+#' @param time A time.
+#' @param ... Items to put in the notification item.
+#'
+#'
+#' @export
+
 notificationItem <- function(icon = NULL, title = NULL, message = NULL, time = NULL, ...) {
   tags$li(
     tags$div(class="dropdown-messages-box",
@@ -10,12 +23,40 @@ notificationItem <- function(icon = NULL, title = NULL, message = NULL, time = N
                       )
              ),
     tags$li(class="dropdown-divider")
-    
+
   )
 }
 
 
-
+#' @title Dashboard notification
+#' @name notification
+#'
+#'
+#' @param icon A icon.
+#' @param label A label
+#' @param status A message.
+#' @param ... Items to put in the notification item.
+#'
+#'
+#'
+#' @seealso \code{\link{notificationItem}}.
+#'
+#' @examples
+#' # Basic dashboard page template
+#' notification(icon = icon("envelope"), label = "1",
+#'notificationItem(
+#'  icon = icon("user", class="fa-3x"),
+#'  title = "novo usuário",
+#'  message = "Foi um criado novo usuário",
+#'  time = "14:41"),
+#'notificationItem(
+#'  icon = icon("user", class="fa-3x"),
+#'  title = "novo usuário",
+#'  message = "Foi um criado novo usuário",
+#'  time = "14:41")
+#')
+#'
+#' @export
 
 notification <- function(..., icon = NULL, label = NULL, status = "warning") {
   labelClass <- paste0("label label-", status)
@@ -27,10 +68,10 @@ notification <- function(..., icon = NULL, label = NULL, status = "warning") {
                  icon,
                  tags$span(class=labelClass, label)
                  ),
-          
+
           tags$ul(class="dropdown-menu dropdown-alerts",
                   ...
                   )
-         
+
           )
 }
