dde645c2772e00f82a07fcc9f357b2e46dd6a7a6
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-26 12:31:28 -0300

    bodyDash - document

--- R/bodyDash.R
+++ R/bodyDash.R
@@ -1,9 +1,20 @@
+#' @title Dashboard Body
+#' @name bodyDash
+#'
+#'
+#' @param ... Items to put in the dashboard body.
+#'
+#'
+#'
+#' @export
+
+
 bodyDash <- function(...) {
   tags$div(class="wrapper wrapper-content",
-            tags$div(class = "tab-content", 
-              
+            tags$div(class = "tab-content",
+
               ...
-              
+
               )
 
            )
