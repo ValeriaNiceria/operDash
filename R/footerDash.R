ee930c96442592cadb919f82f8010be8c8188096
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-26 12:45:51 -0300

    footerDash - document

--- R/footerDash.R
+++ R/footerDash.R
@@ -1,3 +1,16 @@
+#' @title Dashboard Footer
+#' @name footerDash
+#'
+#'
+#' @param textLeft A text footer by left bar
+#' @param textRight A text footer by right bar#'
+#'
+#'
+#'
+#' @export
+
+
+
 footerDash <- function(textLeft = "textLeft", textRight = "textRight") {
   tags$div(class="footer",
            tags$div(class="float-right", textRight),
