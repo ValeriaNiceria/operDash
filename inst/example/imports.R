a09fee7b3cd8a7197a321339dd69196987983f02
ValeriaNiceria <valerianiceria@gmail.com>
2019-06-26 08:31:08 -0300

    start package

---
+++ inst/example/imports.R
@@ -0,0 +1,56 @@
+#########################
+# Carregando os pacotes #
+#########################
+library(shiny)
+library(shinyWidgets)
+library(htmltools)
+library(htmlwidgets)
+library(dplyr)
+library(highcharter)
+
+
+
+
+####################################
+# Caminho das funções de interface #
+####################################
+source("./R/pageDash.R", encoding = "UTF-8")
+source("./R/headDash.R", encoding = "UTF-8")
+source("./R/navbarDash.R", encoding = "UTF-8")
+source("./R/sidebarDash.R", encoding = "UTF-8")
+source("./R/bodyDash.R", encoding = "UTF-8")
+source("./R/tabItem.R", encoding = "UTF-8")
+source("./R/box.R", encoding = "UTF-8")
+source("./R/infoBox.R", encoding = "UTF-8")
+source("./R/elements.R", encoding = "UTF-8")
+source("./R/carousel.R", encoding = "UTF-8")
+source("./R/tabBox.R", encoding = "UTF-8")
+source("./R/panel.R", encoding = "UTF-8")
+source("./R/notifications.R", encoding = "UTF-8")
+source("./R/timeline.R", encoding = "UTF-8")
+source("./R/footerDash.R", encoding = "UTF-8")
+source("./R/footerScriptsDash.R", encoding = "UTF-8")
+
+
+
+
+####################################
+###     Caminho dos exemplos     ###
+####################################
+source("./examples/boxExample.R", encoding = "UTF-8")
+source("./examples/valueBoxElement.R", encoding = "UTF-8")
+source("./examples/buttonsElements.R", encoding = "UTF-8")
+source("./examples/jumbotronElement.R", encoding = "UTF-8")
+source("./examples/alertElement.R", encoding = "UTF-8")
+source("./examples/progressBarElement.R", encoding = "UTF-8")
+source("./examples/carouselElement.R", encoding = "UTF-8")
+source("./examples/panelElement.R", encoding = "UTF-8")
+source("./examples/timelineElement.R", encoding = "UTF-8")
+source("./examples/code/pageDashExample.R", encoding = "UTF-8")
+source("./examples/code/navbarDashExample.R", encoding = "UTF-8")
+source("./examples/code/bodyDashExample.R", encoding = "UTF-8")
+source("./examples/code/boxExample.R", encoding = "UTF-8")
+source("./examples/code/valueBoxExample.R", encoding = "UTF-8")
+source("./examples/code/elementsExample.R", encoding = "UTF-8")
+source("./examples/code/panelExample.R", encoding = "UTF-8") 
+source("./examples/code/timelineExample.R", encoding = "UTF-8")
---
+++ inst/example/ui.R
@@ -0,0 +1,129 @@
+source("./imports.R")
+
+
+navbar <- navbarDash(brand = "operDash",
+                    menuLeft(
+                      menuItem(text = "Box", tabName = "box"),
+                      
+                      menuDropdown(text = "Elements", icon = icon("flask"),
+                                   menuItem(text = "valueBox", tabName = "valueBoxElement"),
+                                   menuItem(text = "Buttons", tabName = "buttonsElement"),
+                                   menuItem(text = "Jumbotron", tabName = "jumbotronElement"),
+                                   menuItem(text = "Alert", tabName = "alertElement"),
+                                   menuItem(text = "progressBar", tabName = "progressBarElement"),
+                                   menuItem(text = "Carousel", tabName = "carouselElement"),
+                                   menuItem(text = "Panel", tabName = "panelElement"),
+                                   menuItem(text = "Timeline", tabName = "timelineElement")
+                                   ),
+                      
+                      menuDropdown(text = "Dash", icon = icon("laptop-code"), 
+                                   menuItem(text = "pageDash", tabName = "codePageDash"),
+                                   menuItem(text = "navbarDash", tabName = "codeNavbarDash"),
+                                   menuItem(text = "bodyDash", tabName = "codeBodyDash"),
+                                   menuItem(text = "box", tabName = "codeBox"),
+                                   menuItem(text = "valueBox", tabName = "codeValueBox"),
+                                   menuItem(text = "elements", tabName = "codeElements"),
+                                   menuItem(text = "panel", tabName = "codePanel"),
+                                   menuItem(text = "timeline", tabName = "codeTimeline")),
+                      menuDropdown(text = "Graphs", icon = icon("chart-line"))),
+                  
+                    
+                    menuRight(
+                      # Notification dropdown
+                      notification(icon = icon("envelope"),
+                                   label = "1",
+                                   notificationItem(
+                                     icon = icon("user", class="fa-3x"),
+                                     title = "novo usuário",
+                                     message = "Foi um criado novo usuário", 
+                                     time = "14:41"),
+                                   notificationItem(
+                                     icon = icon("user", class="fa-3x"),
+                                     title = "novo usuário",
+                                     message = "Foi um criado novo usuário", 
+                                     time = "14:41")
+                                   ),
+                      
+                      notification(icon = icon("envelope"),
+                                   label = "1",
+                                   notificationItem(
+                                     icon = icon("user", class="fa-3x"),
+                                     title = "novo usuário",
+                                     message = "Foi um criado novo usuário", 
+                                     time = "14:41"),
+                                   notificationItem(
+                                     icon = icon("user", class="fa-3x"),
+                                     title = "novo usuário",
+                                     message = "Foi um criado novo usuário", 
+                                     time = "14:41")
+                      ),
+                      
+                      menuItem(text = "Logout", icon = tags$i(class="fas fa-sign-out-alt"))
+                    )
+)
+
+
+body <- bodyDash(
+  # Box
+  boxExample,
+  
+  # Elements - valueBox 
+  valueBoxElement,
+  
+  # Elements - buttons
+  buttonsElement,
+  
+  # Elements - jumbotron
+  jumbotronElement,
+  
+  # Elements - alert
+  alertElement,
+  
+  # Elements - progressBar
+  progressBarElement,
+  
+  # Elements - carousel
+  carouselElement,
+  
+  # Elements - Panel
+  panelElement,
+  
+  # Elements - Timeline
+  timelineElement,
+  
+  # Dash - Code
+  # code pageDash
+  codePageDashExample,
+  
+  # code navbarDash
+  codeNavbarDashExample,
+  
+  # code bodyDash
+  codeBodyDashExample,
+  
+  # code box
+  codeBoxExample,
+  
+  # valueBox
+  codeValueBoxExample,
+  
+  # code Elements
+  codeElementsExample,
+  
+  # code Panel
+  codePanelExample,
+  
+  # code Timeline
+  codeTimelineExample
+  
+)
+
+
+ui <- pageDash(
+  title = "operDash",
+  header = headDash(),
+  navbar = navbar,
+  body = body,
+  footer = footerDash(),
+  footerScripts = footerScriptsDash()
+)
