7c22a8f4239a5e89ccfce2cd94ef211043a00eb9
ValeriaNiceria <valerianiceria@gmail.com>
2019-07-30 14:21:35 -0300

    removido comentário

--- R/operPageDash.R
+++ R/operPageDash.R
@@ -26,21 +26,6 @@ operPage <- function(title = "", sidebar = NULL, navbar = NULL, body = NULL) {
 }
 
 
-#' @title Dashboard Page
-#' @name sidebarPage
-#'
-#'
-#' @param title Um título que será utilizado no display do browser
-#' @param sidebar Um menu lateral \code{sidebarDash}.
-#' @param body O conteúdo da página \code{bodyDash}.
-#' @param ... Itens adicionais.
-#'
-#'
-#' @seealso \code{\link{sidebarDash}}, \code{\link{navbarDash}}, \code{\link{bodyDash}}.
-#'
-#'
-#' @export
-
 
 sidebarPage <- function(title = NULL, sidebar = NULL, content = NULL) {
 
@@ -180,22 +165,6 @@ sidebarPage <- function(title = NULL, sidebar = NULL, content = NULL) {
 }
 
 
-
-
-#' @title Dashboard Page
-#' @name navbarPage
-#'
-#'
-#' @param title Um título que será utilizado no display do browser
-#' @param navbar Um menu Top \code{navbarDash}.
-#' @param body O conteúdo da página \code{bodyDash}.
-#' @param ... Itens adicionais.
-#'
-#'
-#' @seealso \code{\link{sidebarDash}}, \code{\link{navbarDash}}, \code{\link{bodyDash}}.
-#'
-#'
-#' @export
 
 
 navbarPage <- function(title = NULL, navbar = NULL, content = NULL) {
