12eb2c063dd523b788fe3f67d7e6364f91e50004
ValeriaNiceria <valerianiceria@gmail.com>
2019-08-04 19:46:37 -0300

    update

--- R/sidebarDash.R
+++ R/sidebarDash.R
@@ -75,15 +75,15 @@ sidebarDash <- function(..., imgUser = NULL, userOptions = NULL, brand = NULL) {
               width = "60px"
             )
           ),
-          shiny::tags$div(
-            class="logo-element",
-            shiny::tags$img(
-              alt="image",
-              class="rounded-circle",
-              src=imgUser,
-              width = "35px"
-            )
-          ),
+          # shiny::tags$div(
+          #   class="logo-element",
+          #   shiny::tags$img(
+          #     alt="image",
+          #     class="rounded-circle",
+          #     src=imgUser,
+          #     width = "35px"
+          #   )
+          # ),
 
           userOptions
 
