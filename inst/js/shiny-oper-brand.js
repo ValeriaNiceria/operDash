56ddbe5b45fbcdfdc99200add5516692e7e770d2
ValeriaNiceria <valerianiceria@gmail.com>
2019-08-20 22:46:57 -0300

    brand image

--- inst/js/shiny-oper-brand.js
+++ inst/js/shiny-oper-brand.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   $(document).on('click', '.navbar-header', function () {
     body = $('body');
     if (body.hasClass('mini-navbar')) {
-      $('.oper-brand').css('display', 'none').animate({opacity:1},1000);
+      $('.oper-brand').css('display', 'none').animate({opacity:1},1300);
     } else {
-      $('.oper-brand').css('display', 'block').animate({opacity:1},1000);
+      $('.oper-brand').css('display', 'block').animate({opacity:1},1300);
     }
   });
 });
--- test/ui.R
+++ test/ui.R
@@ -140,7 +140,7 @@ ui =  operPage(
     sidebarItem(text = "Box", tabName = "box"),
 
 
-    brand = h1('testando')
+    brand = img(src="https://raw.githubusercontent.com/ValeriaNiceria/operDash/master/inst/img/logo.png", width = "150")
   ),
 
 
