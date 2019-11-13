6970170204f51a787140eb43700e30b2b86c773e
ValeriaNiceria <valerianiceria@gmail.com>
2019-11-13 14:07:31 -0300

    infoBox

--- R/widgets.R
+++ R/widgets.R
@@ -1,26 +1,50 @@
 #' @title infoBox
 #' @name infoBox
 #'
-#' @description descrição
+#' @description Uma caixa de informação.
 #'
 #' @param width Largura da box, podendo ser utilizados os valores entre 1 e 12.
-#' @param bg Cor de fundo do ícone, podendo ser utilizado os valores: aqua, green, yellow e red.
+#' @param bg_color Cor de fundo do ícone, podendo ser utilizado os valores: aqua, green, yellow e red.
 #' @param icon Ícone
 #' @param text Texto
 #' @param value Valor
-#'
+#' @param progress Número da linha de progresso, podendo ser de 1 à 100.
+#' @param progress_desc Descrição que será adicionado abaixo da linha de progresso.
+#' @param bg_all Pode-se passar o valor TRUE ou FALSE, se deseja que a box inteira fique colorida.
 #'
 #' @export
-infoBox <- function(width = 12, bg = "aqua", icon = NULL, text = NULL, value = NULL) {
+infoBox <- function(width = 12, bg_color = "aqua", icon = NULL, text = NULL, value = NULL, progress = NULL, progress_desc = NULL, bg_all = FALSE) {
   width = paste0("col-sm-", width)
-  bg_color = paste0("bg-", bg)
+  bg_color = paste0("bg-", bg_color)
+  progress = if (!is.null(progress)) paste0("width: ", progress, "%")
+  progress_bar = NULL
+  bg_all = if (isTRUE(bg_all)) bg_color
+  bg_icon = if (isFALSE(bg_all)) bg_color
+  bg_icon = if (is.null(bg_all)) bg_color
+
+  if (!is.null(progress_desc)) {
+    progress_desc = shiny::tags$span(
+      class="progress-description",
+      progress_desc
+    )
+  }
+
+  if (!is.null(progress)) {
+    progress_bar = shiny::tags$div(
+      class="progress",
+      shiny::tags$div(
+        class="progress-bar",
+        style=progress
+      )
+    )
+  }
 
   shiny::tags$div(
     class=width,
     shiny::tags$div(
-      class="info-box",
+      class=paste("info-box", bg_all),
       shiny::tags$span(
-        class=paste("info-box-icon", bg_color),
+        class=paste("info-box-icon", bg_icon),
         icon
       ),
 
@@ -33,9 +57,21 @@ infoBox <- function(width = 12, bg = "aqua", icon = NULL, text = NULL, value = N
         shiny::tags$span(
           class="info-box-number",
           value
-        )
+        ),
+
+        progress_bar,
+        progress_desc
+
       )
     )
   )
 
 }
+
+
+
+
+
+
+
+
--- example/ui.R
+++ example/ui.R
@@ -49,9 +49,9 @@ ui <- operDashPage(
       h3("infoBox"),
       fluidRow(
         infoBox(width = 3, icon = icon("envelope-o"), text = "Message", value = "500"),
-        infoBox(width = 3, bg ="red", icon = icon("envelope-o"), text = "Message", value = "500"),
-        infoBox(width = 3, bg ="green", icon = icon("envelope-o"), text = "Message", value = "500"),
-        infoBox(width = 3, bg ="yellow", icon = icon("envelope-o"), text = "Message", value = "500")
+        infoBox(width = 3, bg_color ="red", icon = icon("envelope-o"), text = "Message", value = "500"),
+        infoBox(width = 3, bg_color ="green", icon = icon("envelope-o"), text = "Message", value = "500"),
+        infoBox(width = 3, bg_color ="yellow", bg_all = TRUE, icon = icon("envelope-o"), text = "Message", value = "500", progress = "45", progress_desc = "descrição progresso")
       )
     )
   )
